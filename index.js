645311c13296e8ee41869411d78617b3e32e0ae8
Damien Jones <damienjones@gmail.com>
2020-06-25 02:20:29 -0700

    Complete seed script

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3002
+
+app.listen(port, () => console.log(`Steam user tags service. listening at http://localhost:${port}`))
+app.use(express.static('./client/dist'));
+
+// SAMPLE API CALLS
+
+app.get('/api/tags/:productId', ( req, res) => {
+    res.send( sampleTags );
+});
+
+var sampleTags = ['Strategy', 'Turn-Based Strategy', 'Historical', 'Multiplayer', 'Singleplayer', 'Turn-Based', 'Grand Strategy', '4X', 'War', 'Simulation', 'Tactical', 'City Builder', 'Great Soundtrack', 'Moddable', 'Online Co-Op', 'Co-op', 'Building', 'Management', 'Hex Grid', 'Atmospheric'];
