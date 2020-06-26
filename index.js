005f75a9749cd2b7dc551152a767b37f8a8afb8e
Damien Jones <damienjones@gmail.com>
2020-06-26 01:11:19 -0700

    spacing changes

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = 3002
 app.listen(port, () => console.log(`Steam user tags service. listening at http://localhost:${port}`))
 app.use(express.static('./client/dist'));
 
-app.get('/api/tags/:gameId', ( req, res) => {
+app.get('/api/tags/:gameId', ( req, res ) => {
 
     // get all user tags for this game sorted by popularity ( most tagged first )
     var sqlText = `SELECT name FROM user_tags WHERE gameid = ${ req.params.gameId } ORDER BY count DESC`;
 
-    db.query( sqlText, (err, result, fields) => {
+    db.query( sqlText, ( err, result, fields ) => {
         if (err) throw err;
         res.send( result.map( row => row['name'] )  );
         // ex. { ['Strategy', 'Turn-Based Strategy', 'Historical', 'Multiplayer', ... ] }
