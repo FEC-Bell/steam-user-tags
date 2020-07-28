407a94f53724f680975133dd98e44fe5256e4cf8
Damien Jones <damienjones@gmail.com>
2020-07-28 16:12:42 -0700

    fixing up proxy parameters for AWS

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Tags from './Tags';
 
 const slashes = window.location.pathname.split('/');
-const gameid = slashes[slashes.length - 1].split('?')[0].split('#')[0];
+const gameid = slashes[slashes.length - 1].split('?')[0].split('#')[0] || 1;
 
 ReactDOM.render(<Tags game={gameid} />, document.getElementById('user-tags'));
