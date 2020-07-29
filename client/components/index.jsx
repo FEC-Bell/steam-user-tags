59c6b5862794486a8d56a6b6fa8e33fc9b9e28b2
Damien Jones <damienjones@gmail.com>
2020-07-28 18:45:29 -0700

    fixing server paths to the right one

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -5,7 +5,7 @@ import Tags from './Tags';
 const slashes = window.location.pathname.split('/');
 const gameId = (slashes[slashes.length - 1].split('?')[0].split('#')[0]) || 1;
 
-let serverRoot = 'http://44.233.13.178:3002';
-// serverRoot = 'http://localhost:3002'; // comment out for prod
+let serverRoot = 'http://44.233.13.178:3006';
+// serverRoot = 'http://localhost:3006'; // comment out for prod
 
 ReactDOM.render(<Tags game={gameId} server={serverRoot}/>, document.getElementById('user-tags'));
